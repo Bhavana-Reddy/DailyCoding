17ef38cf229322bfae999c752902d09f91b63144
bhavanpa <pajjuri.bhavana@adp.com>
2020-02-26 11:25:41 +0530

    java loops problems

--- CodeForWin/src/LoopsAndIterationExercises/FrequencyOfEachDigitInANumber.java
+++ CodeForWin/src/LoopsAndIterationExercises/FrequencyOfEachDigitInANumber.java
@@ -1,5 +1,27 @@
 package LoopsAndIterationExercises;
 
+import java.util.Scanner;
+
 public class FrequencyOfEachDigitInANumber {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter n -");
+		int n = s.nextInt();
+		
+		//Using an array to provide 0-9 digits frequency 
+		int [] freq = new int[10];
+		
+		int mydigit;
+		
+		while(n>0) {
+			mydigit = n%10;
+			freq[mydigit]++;
+			n=n/10;
+		}
+		
+		for(int i=0;i<freq.length;i++) {
+			System.out.println("The freq of"+i+" is  "+freq[i]+" ");
+		}
+	}
 
 }
