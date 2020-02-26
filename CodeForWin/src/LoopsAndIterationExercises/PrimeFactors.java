e517d5e961c18c7a4f887b4a584cf0608c85e275
bhavanpa <pajjuri.bhavana@adp.com>
2020-02-26 23:44:39 +0530

    codeforwin loops problems

---
+++ CodeForWin/src/LoopsAndIterationExercises/PrimeFactors.java
@@ -0,0 +1,13 @@
+package LoopsAndIterationExercises;
+
+import java.util.Scanner;
+
+public class PrimeFactors {
+	public static void main(String[] args) {
+		Scanner  s = new Scanner(System.in);
+		System.out.println("Enter n - ");
+		int n = s.nextInt();
+		
+	}
+
+}
---
+++ CodeForWin/src/LoopsAndIterationExercises/PrimeOrNot.java
@@ -0,0 +1,22 @@
+package LoopsAndIterationExercises;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter n -");
+		int n = s.nextInt();
+		
+		for(int i=2;i<n;i++) {
+			if(n%i==0) {
+				System.out.println("Number is not prime");
+				break;
+			}else {
+				System.out.println("Number is a prime");
+				break;
+			}
+		}
+	}
+
+}
